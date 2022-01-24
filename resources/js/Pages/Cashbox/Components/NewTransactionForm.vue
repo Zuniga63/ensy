a35882cae970a3b2d3f9c15efee00b2916901855
Andrés Zúñiga <andres.zuniga.063@gmail.com>
2022-01-24 17:36:43 -0500

    add spin to form

--- resources/js/Pages/Cashbox/Components/NewTransactionForm.vue
+++ resources/js/Pages/Cashbox/Components/NewTransactionForm.vue
@@ -207,7 +207,30 @@
         border-t-4 border-double border-slate-900
       "
     >
-      <JetButton> {{ buttonMessage }} </JetButton>
+      <JetButton>
+        <svg
+          class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          v-show="processing"
+        >
+          <circle
+            class="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            stroke-width="4"
+          ></circle>
+          <path
+            class="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+          ></path>
+        </svg>
+        {{ buttonMessage }}
+      </JetButton>
       <JetDangerButton type="button" @click="hiddenForm"
         >Cancelar</JetDangerButton
       >
@@ -237,7 +260,7 @@ export default {
       type: Number,
     },
     maxDate: {
-      type: String
+      type: String,
     },
   },
   setup(props) {
@@ -257,6 +280,7 @@ export default {
   data() {
     return {
       buttonMessage: "Registrar",
+      processing: false,
     };
   },
   methods: {
@@ -270,12 +294,14 @@ export default {
         preserveState: true,
         onStart: () => {
           this.buttonMessage = "Registrando...";
+          this.processing = true;
         },
         onSuccess: (page) => {
           this.hiddenForm();
         },
         onFinish: () => {
           this.buttonMessage = "Registrar";
+          this.processing = false;
         },
       });
     },
