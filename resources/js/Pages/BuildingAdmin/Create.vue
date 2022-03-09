d2b52f74e99feca80daf9b21c279ea448993be25
Andrés Zúñiga <andres.zuniga.063@gmail.com>
2022-03-09 14:34:20 -0500

    separate the field of building of admin

--- resources/js/Pages/BuildingAdmin/Create.vue
+++ resources/js/Pages/BuildingAdmin/Create.vue
@@ -71,113 +71,184 @@
             <jet-input-error :message="form.errors.address" class="mt-2" />
           </div>
 
-          <!-- Nombre del administrador -->
-          <div class="col-span-6 lg:col-span-2">
-            <!-- Etiqueta -->
-            <custom-label
-              for="adfirst_name"
-              class="mb-2"
-              value="Nombres del Administrador"
-            />
-            <!-- Input -->
-            <jet-input
-              id="adfirst_name"
-              name="adfirst_name"
-              type="text"
-              v-model="form.admin_first_name"
-              class="w-full"
-              placeholder="Escribe el nombre aquí."
-            />
+          <input-group title="Información del Administrador" class="col-span-6">
+            <div class="grid grid-cols-6 gap-4">
+              <!-- Nombre del administrador -->
+              <div class="col-span-6 lg:col-span-2">
+                <!-- Etiqueta -->
+                <custom-label for="adfirst_name" class="mb-2" value="Nombres" />
+                <!-- Input -->
+                <jet-input
+                  id="adfirst_name"
+                  name="adfirst_name"
+                  type="text"
+                  v-model="form.admin_first_name"
+                  class="w-full"
+                  placeholder="Escribe el nombre aquí."
+                />
 
-            <!-- Error -->
-            <jet-input-error :message="form.errors.admin_first_name" class="mt-2" />
-          </div>
+                <!-- Error -->
+                <jet-input-error
+                  :message="form.errors.admin_first_name"
+                  class="mt-2"
+                />
+              </div>
 
-          <!-- Apellidos del adminsitrador -->
-          <div class="col-span-6 lg:col-span-2">
-            <!-- Etiqueta -->
-            <custom-label
-              for="admin-last-name"
-              class="mb-2"
-              value="Apellidos del Administrador"
-            />
-            <!-- Input -->
-            <jet-input
-              id="admin-last-name"
-              name="admin-last-name"
-              type="text"
-              v-model="form.admin_last_name"
-              class="w-full"
-              placeholder="Escribe los apellidos aquí."
-            />
+              <!-- Apellidos del adminsitrador -->
+              <div class="col-span-6 lg:col-span-2">
+                <!-- Etiqueta -->
+                <custom-label
+                  for="admin-last-name"
+                  class="mb-2"
+                  value="Apellidos"
+                />
+                <!-- Input -->
+                <jet-input
+                  id="admin-last-name"
+                  name="admin-last-name"
+                  type="text"
+                  v-model="form.admin_last_name"
+                  class="w-full"
+                  placeholder="Escribe los apellidos aquí."
+                />
 
-            <!-- Error -->
-            <jet-input-error :message="form.errors.admin_last_name" class="mt-2" />
-          </div>
+                <!-- Error -->
+                <jet-input-error
+                  :message="form.errors.admin_last_name"
+                  class="mt-2"
+                />
+              </div>
 
-          <!-- Documento -->
-          <div class="col-span-6 lg:col-span-2">
-            <!-- Etiqueta -->
-            <custom-label
-              for="admin-document_number"
-              class="mb-2"
-              value="Documento de Identificación"
-            />
-            <!-- Input -->
-            <jet-input
-              id="admin-document_number"
-              name="admin-document_number"
-              type="text"
-              v-model="form.admin_document_number"
-              class="w-full"
-              placeholder="Escribe el documento aquí."
-            />
+              <!-- Documento -->
+              <div class="col-span-6 lg:col-span-2">
+                <!-- Etiqueta -->
+                <custom-label
+                  for="admin-document_number"
+                  class="mb-2"
+                  value="N° de Documento"
+                />
+                <!-- Input -->
+                <jet-input
+                  id="admin-document_number"
+                  name="admin-document_number"
+                  type="text"
+                  v-model="form.admin_document_number"
+                  class="w-full"
+                  placeholder="Escribe el documento aquí."
+                />
 
-            <!-- Error -->
-            <jet-input-error
-              :message="form.errors.admin_document_number"
-              class="mt-2"
-            />
-          </div>
+                <!-- Error -->
+                <jet-input-error
+                  :message="form.errors.admin_document_number"
+                  class="mt-2"
+                />
+              </div>
 
-          <!-- Correo Electronico -->
-          <div class="col-span-6 lg:col-span-3">
-            <!-- Etiqueta -->
-            <custom-label
-              for="admin-email"
-              class="mb-2"
-              value="Correo Electronico"
-            />
-            <!-- Input -->
-            <jet-input
-              id="admin-email"
-              name="admin-email"
-              type="email"
-              v-model="form.email"
-              class="w-full"
-              placeholder="ejemplo@ejemplo.com"
-            />
+              <!-- Correo Electronico -->
+              <div class="col-span-6 lg:col-span-3">
+                <!-- Etiqueta -->
+                <custom-label
+                  for="admin-email"
+                  class="mb-2"
+                  value="Correo Electronico"
+                />
+                <!-- Input -->
+                <jet-input
+                  id="admin-email"
+                  name="admin-email"
+                  type="email"
+                  v-model="form.email"
+                  class="w-full"
+                  placeholder="ejemplo@ejemplo.com"
+                />
 
-            <!-- Error -->
-            <jet-input-error :message="form.errors.email" class="mt-2" />
-          </div>
+                <!-- Error -->
+                <jet-input-error :message="form.errors.email" class="mt-2" />
+              </div>
 
-          <!-- Telefono -->
-          <div class="col-span-6 lg:col-span-3">
-            <!-- Etiqueta -->
-            <custom-label for="admin-phone" class="mb-2" value="Telefono" />
-            <!-- Input -->
-            <div class="flex mb-2 items-center">
-              <!-- Numero de telefono -->
-              <jet-input
-                id="admin-phone"
-                name="admin-phone"
-                type="tel"
-                v-model="phoneNumber"
-                class="w-full mr-2"
-                placeholder="555-555-5555"
-              />
-              <!-- Transición para extensiones -->
+              <!-- Telefono -->
+              <div class="col-span-6 lg:col-span-3">
+                <!-- Etiqueta -->
+                <custom-label for="admin-phone" class="mb-2" value="Telefono" />
+                <!-- Input -->
+                <div class="flex mb-2 items-center">
+                  <!-- Numero de telefono -->
+                  <jet-input
+                    id="admin-phone"
+                    name="admin-phone"
+                    type="tel"
+                    v-model="phoneNumber"
+                    class="w-full mr-2"
+                    placeholder="555-555-5555"
+                  />
+                  <!-- Transición para extensiones -->
+                  <transition
+                    enter-active-class="transition ease-out duration-200"
+                    enter-from-class="opacity-0 scale-90"
+                    enter-to-class="opacity-100 scale-100"
+                    leave-active-class="transition ease-in duration-200"
+                    leave-from-class="opacity-100 scale-100"
+                    leave-to-class="opacity-0 scale-90"
+                  >
+                    <!-- Contendor de Extensiones -->
+                    <div class="flex mr-2" v-show="hasExtension">
+                      <!-- Extensión #1 -->
+                      <jet-input
+                        type="text"
+                        class="w-20 mr-2"
+                        placeholder="EXT-1"
+                        v-model="ext1"
+                      />
+                      <!-- Extensión #2 -->
+                      <jet-input
+                        type="text"
+                        class="w-20"
+                        placeholder="EXT-2"
+                        v-model="ext2"
+                      />
+                    </div>
+                  </transition>
+
+                  <!-- Boton para agregar telefono al listado -->
+                  <button
+                    type="button"
+                    class="
+                      py-2
+                      px-3
+                      border border-blue-200
+                      rounded-md
+                      text-sm text-white
+                      font-bold
+                      bg-blue-500
+                      shadow
+                      hover:bg-white
+                      hover:text-gray-800
+                      hover:ring
+                      hover:ring-blue-500
+                      hover:ring-opacity-20
+                    "
+                    @click="addPhone"
+                  >
+                    +
+                  </button>
+                </div>
+
+                <!-- Error -->
+                <jet-input-error :message="form.errors.phones" class="mt-2" />
+                <!-- Checked -->
+                <div class="flex items-center">
+                  <JetCheckbox
+                    class="mx-2"
+                    v-model:checked="hasExtension"
+                    id="business-show-email"
+                  />
+                  <label for="business-show-email" class="text-sm text-gray-800"
+                    >Tiene Extensiones</label
+                  >
+                </div>
+              </div>
+
+              <!-- Listado de telefonos -->
               <transition
                 enter-active-class="transition ease-out duration-200"
                 enter-from-class="opacity-0 scale-90"
@@ -186,142 +257,77 @@
                 leave-from-class="opacity-100 scale-100"
                 leave-to-class="opacity-0 scale-90"
               >
-                <!-- Contendor de Extensiones -->
-                <div class="flex mr-2" v-show="hasExtension">
-                  <!-- Extensión #1 -->
-                  <jet-input
-                    type="text"
-                    class="w-20 mr-2"
-                    placeholder="EXT-1"
-                    v-model="ext1"
-                  />
-                  <!-- Extensión #2 -->
-                  <jet-input
-                    type="text"
-                    class="w-20"
-                    placeholder="EXT-2"
-                    v-model="ext2"
-                  />
-                </div>
-              </transition>
-
-              <!-- Boton para agregar telefono al listado -->
-              <button
-                type="button"
-                class="
-                  py-2
-                  px-3
-                  border border-blue-200
-                  rounded-md
-                  text-sm text-white
-                  font-bold
-                  bg-blue-500
-                  shadow
-                  hover:bg-white
-                  hover:text-gray-800
-                  hover:ring
-                  hover:ring-blue-500
-                  hover:ring-opacity-20
-                "
-                @click="addPhone"
-              >
-                +
-              </button>
-            </div>
-
-            <!-- Error -->
-            <jet-input-error :message="form.errors.phones" class="mt-2" />
-            <!-- Checked -->
-            <div class="flex items-center">
-              <JetCheckbox
-                class="mx-2"
-                v-model:checked="hasExtension"
-                id="business-show-email"
-              />
-              <label for="business-show-email" class="text-sm text-gray-800"
-                >Tiene Extensiones</label
-              >
-            </div>
-          </div>
-
-          <!-- Listado de telefonos -->
-          <transition
-            enter-active-class="transition ease-out duration-200"
-            enter-from-class="opacity-0 scale-90"
-            enter-to-class="opacity-100 scale-100"
-            leave-active-class="transition ease-in duration-200"
-            leave-from-class="opacity-100 scale-100"
-            leave-to-class="opacity-0 scale-90"
-          >
-            <div
-              v-show="phones.length > 0"
-              class="
-                relative
-                col-span-6
-                border border-gray-200
-                rounded
-                px-4
-                pt-6
-                pb-4
-              "
-            >
-              <!-- Pill -->
-              <p
-                class="
-                  absolute
-                  top-0
-                  inline-block
-                  px-4
-                  text-sm text-gray-800
-                  border border-gray-200
-                  rounded-full
-                  bg-white
-                  transform
-                  -translate-y-1/2
-                "
-              >
-                Listado de Telefonos
-              </p>
-              <!-- Listado -->
-              <ul class="flex flex-wrap">
-                <li
-                  v-for="(item, index) in phones"
-                  :key="index"
+                <div
+                  v-show="phones.length > 0"
                   class="
-                    flex
-                    items-center
-                    px-4
-                    py-2
-                    mr-2
-                    mb-2
+                    relative
+                    col-span-6
                     border border-gray-200
-                    rounded-sm
-                    hover:border-gray-400 hover:cursor-pointer
+                    rounded
+                    px-4
+                    pt-6
+                    pb-4
                   "
                 >
-                  <div class="pr-2 mr-2 border-r border-gray-200 text-sm">
-                    <!-- Numero -->
-                    {{ item.number }}
-                    <!-- Extensiones -->
-                    <span
-                      v-for="(ext, index) in item.exts"
-                      :key="index"
-                      class="text-gray-400"
-                    >
-                      - (ext: {{ ext }})
-                    </span>
-                  </div>
-
-                  <a
-                    href="javascript:;"
-                    class="text-sm text-gray-400 hover:text-gray-700"
-                    @click="removePhone(index)"
-                    >x</a
+                  <!-- Pill -->
+                  <p
+                    class="
+                      absolute
+                      top-0
+                      inline-block
+                      px-4
+                      text-sm text-gray-800
+                      border border-gray-200
+                      rounded-full
+                      bg-white
+                      transform
+                      -translate-y-1/2
+                    "
                   >
-                </li>
-              </ul>
+                    Listado de Telefonos
+                  </p>
+                  <!-- Listado -->
+                  <ul class="flex flex-wrap">
+                    <li
+                      v-for="(item, index) in phones"
+                      :key="index"
+                      class="
+                        flex
+                        items-center
+                        px-4
+                        py-2
+                        mr-2
+                        mb-2
+                        border border-gray-200
+                        rounded-sm
+                        hover:border-gray-400 hover:cursor-pointer
+                      "
+                    >
+                      <div class="pr-2 mr-2 border-r border-gray-200 text-sm">
+                        <!-- Numero -->
+                        {{ item.number }}
+                        <!-- Extensiones -->
+                        <span
+                          v-for="(ext, index) in item.exts"
+                          :key="index"
+                          class="text-gray-400"
+                        >
+                          - (ext: {{ ext }})
+                        </span>
+                      </div>
+
+                      <a
+                        href="javascript:;"
+                        class="text-sm text-gray-400 hover:text-gray-700"
+                        @click="removePhone(index)"
+                        >x</a
+                      >
+                    </li>
+                  </ul>
+                </div>
+              </transition>
             </div>
-          </transition>
+          </input-group>
         </template>
 
         <template #actions>
@@ -360,6 +366,7 @@ import JetActionMessage from "@/Jetstream/ActionMessage.vue";
 import JetCheckbox from "@/Jetstream/Checkbox.vue";
 import LinkButton from "@/Components/Form/LinkButton.vue";
 import { useForm } from "@inertiajs/inertia-vue3";
+import InputGroup from "@/Components/Form/InputGroupSection.vue";
 
 export default {
   components: {
@@ -372,6 +379,7 @@ export default {
     CustomLabel,
     JetCheckbox,
     LinkButton,
+    InputGroup,
   },
   setup(props) {
     const form = useForm({
@@ -453,7 +461,7 @@ export default {
         preserveScroll: true,
         onSuccess: (page) => {
           let res = page.props.flash.message;
-          if(res.ok && res.reload){
+          if (res.ok && res.reload) {
             this.form.reset();
             this.form.addOtherAdmin = true;
             this.phones = [];
