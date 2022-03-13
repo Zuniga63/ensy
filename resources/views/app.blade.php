3f3f75b8b9961fc9c696c6fc5ff89456d4ca77e6
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-03-13 14:19:33 -0500

    change tipography to poppins

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,12 +7,16 @@
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
   @if (config('app.favicon'))
-    <link rel="icon" type="image/png" href="{{ config('app.favicon') }}" />  
+  <link rel="icon" type="image/png" href="{{ config('app.favicon') }}" />
   @endif
   <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
   <!-- Fonts -->
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+  <!--link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap"-->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <!--link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@400;600;700&display=swap" rel="stylesheet"-->
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,600;0,700;1,400&display=swap" rel="stylesheet">
 
   <!-- Styles -->
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
