06776c2e5e2e8d2164f500718e459699e82bc8c5
Andres Zuñiga <andres.zuniga.063@gmail.com>
2022-04-12 06:59:48 -0500

    add connection to carmú database

--- config/database.php
+++ config/database.php
@@ -63,6 +63,27 @@ return [
             ]) : [],
         ],
 
+        'carmu' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('CARMU_HOST', '127.0.0.1'),
+            'port' => env('CARMU_PORT', '3306'),
+            'database' => env('CARMU_DATABASE', 'forge'),
+            'username' => env('CARMU_USERNAME', 'forge'),
+            'password' => env('CARMU_PASSWORD', ''),
+            'unix_socket' => env('CARMU_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
+
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
@@ -123,7 +144,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
